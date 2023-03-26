ad8933ad5bbb3d9d23ad37179d9e25aa053e1159
dlgusdn <gusdn010158@gmail.com>
2023-03-26 23:59:45 +0900

    fcommit

--- src/components/Products/Productsmain.js
+++ src/components/Products/Productsmain.js
@@ -1,9 +1,56 @@
 import React from 'react';
 import './Productsmain.css';
+import advertisement from '../img/advertisement.png';
 function Productsmain() {
     return (
         <div className="Productsmain">
-            쇼핑몰 메인 페이지입니다.
+            <div className="advertisement">
+                <img src={advertisement} alt="advertisement"/>
+                <h1>BEST</h1>
+                <div className="main_best"> 
+                <div className="main_best1">
+                    <div className="main_img1"></div>
+                    <div className="main_text1"><h2>상품 이름</h2> 가격:45121원</div>
+                </div>
+                <div className="main_best2">
+                <div className="main_img2"></div>
+                    <div className="main_text2"><h2>상품 이름</h2> 가격:45121원</div>
+                </div>
+                <div className="main_best3">
+                <div className="main_img3"></div>
+                    <div className="main_text3"><h2>상품 이름</h2> 가격:45121원</div>
+                </div>
+                <div className="main_best4">
+                <div className="main_img4"></div>
+                    <div className="main_text4"><h2>상품 이름</h2> 가격:45121원</div>
+                </div>
+                <div className="main_best5">
+                <div className="main_img5"></div>
+                    <div className="main_text5"><h2>상품 이름</h2> 가격:45121원</div>
+                </div>
+                <div className="main_best5">
+                <div className="main_img5"></div>
+                    <div className="main_text5"><h2>상품 이름</h2> 가격:45121원</div>
+                </div>
+                <div className="main_best5">
+                <div className="main_img5"></div>
+                    <div className="main_text5"><h2>상품 이름</h2> 가격:45121원</div>
+                </div>
+                <div className="main_best5">
+                <div className="main_img5"></div>
+                    <div className="main_text5"><h2>상품 이름</h2> 가격:45121원</div>
+                </div>
+                <div className="main_best5">
+                <div className="main_img5"></div>
+                    <div className="main_text5"><h2>상품 이름</h2> 가격:45121원</div>
+                </div>
+                <div className="main_best5">
+                <div className="main_img5"></div>
+                    <div className="main_text5"><h2>상품 이름</h2> 가격:45121원</div>
+                </div>
+
+                </div>
+            </div>
         </div>
     );
 }
